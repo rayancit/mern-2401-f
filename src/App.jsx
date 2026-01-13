1d10c1692987fb6267371b5218b387b511a9c5c8
Md Mohibbullah <mohammedhanif74612@gmail.com>
2026-01-13 13:01:24 +0200

    feat: tested

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Mohib from "./components/Mohib";
+
 function App() {
   return (
     <>
-      <h1>hello world</h1>
+      <Mohib />
     </>
   );
 }
---
+++ src/components/Mohib.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Mohib = () => {
+  return <div>HEllo this is a simple pulling</div>;
+};
+
+export default Mohib;
